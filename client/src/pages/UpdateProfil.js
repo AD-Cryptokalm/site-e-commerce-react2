914c45b2a6357eac98933557b8c13ec69df264d0
AD-Cryptokalm <cryptokalm82@gmail.com>
2022-12-16 00:58:42 +0100

    update profil

--- client/src/pages/UpdateProfil.js
+++ client/src/pages/UpdateProfil.js
@@ -1,106 +1,74 @@
-// import axios from "axios";
+import axios from "axios";
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  updateAdress,
-  updateEmail,
-  updateFirstname,
-} from "../actions/userAction";
+// import axios from 'axios'
+// import {
+//   updateAdress,
+//   updateEmail,
+//   updateFirstname,
+// } from "../actions/userAction";
 // import { UidContext } from "../components/context/authContext";
 import "../styles/updateProfil.scss";
+import { updateUser } from "../actions/userAction";
 
 export default function UpdateProfil() {
   const userData = useSelector((state) => state.userReducer);
   // const token = useContext(UidContext);
-  // const userId = localStorage.getItem("uid");
+  const token = localStorage.getItem("token");
   const [email, setEmail] = useState("");
-  const [firstname, setFirstname] = useState("");
-  const [lastname, setLastname] = useState("");
-  const [adress, setAdress] = useState("");
-  const [postalCode, setPostalCode] = useState("");
-  const [city, setCity] = useState("");
+  const [firstname, setFirstname] = useState(userData.firstname);
+  const [lastname, setLastname] = useState(userData.lastname);
+  const [password, setPassword] = useState("")
+  const [adress, setAdress] = useState(userData.adress);
+  const [postalCode, setPostalCode] = useState(userData.setPostalCode);
+  const [city, setCity] = useState(userData.city);
   const dispatch = useDispatch();
 
-  const handleChangeFirstname = () => {
-    dispatch(updateFirstname(userData._id, firstname));
+  const handleChange = () => {
+    // e.preventDefault();
+    axios({
+      method: "PUT",
+      url: `http://localhost:3000/users/${userData.id}`,
+      withCredentials: true,
+      data: {
+        firstname,
+        lastname,
+        password,
+        email,
+        adress,
+        postalCode,
+        city,
+      },
+      headers: {
+        content_type: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => dispatch(updateUser(userData.id, res)))
+      .catch((err) => console.log(err));
   };
-  const handleChangeLastname = () => {
-    dispatch(updateFirstname(userData._id, lastname));
-  };
-  const handleChangeEmail = () => {
-    dispatch(updateEmail(userData._id, email));
-  };
-  const handleChangeAdress = () => {
-    dispatch(updateAdress(userData._id, adress));
-  };
-  const handleChangePostalCode = () => {
-    dispatch(updateAdress(userData._id, postalCode));
-  };
-  const handleChangeCity = () => {
-    dispatch(updateAdress(userData._id, city));
-  };
-
-  // const handleChange = (e) => {
-  //   e.preventDefault();
-  //   axios({
-  //     method: "PUT",
-  //     url: `http://localhost:3000/660/users/${userId}`,
-  //     withCredentials: true,
-  //     data: {
-  //       firstname,
-  //       lastname,
-  //       email,
-  //       adress,
-  //       postalCode,
-  //       city,
-  //     },
-  //     headers: {
-  //       content_type: "application/json",
-  //       Authorization: `Bearer ${token}`,
-  //     },
-  //   })
-  //     .then((res) => console.log(res))
-  //     .catch((err) => console.log(err));
-  // };
 
   return (
     <div className="updateProfil">
-          <h2>Modifiez votre profil</h2>
-      <form action="" onSubmit={handleChangeFirstname} className="mini-form">
-            <input
-              type="text"
-              name="firstname"
-              id="firstname"
-              onChange={(e) => setFirstname(e.target.value)}
-              value={firstname}
+      <h2>Modifiez votre profil</h2>
+      <form action="" onSubmit={handleChange}>
+        <input
+          type="text"
+          name="firstname"
+          id="firstname"
+          onChange={(e) => setFirstname(e.target.value)}
+          value={firstname}
           placeholder={userData.firstname}
-            />
-        <input className="btn-form" type="submit" value="Valider" />
-      </form>
-      <form action="" onSubmit={handleChangeLastname} className="mini-form">
-            <input
-              type="text"
-              name="lastname"
-              id="lastname"
-              onChange={(e) => setLastname(e.target.value)}
-              value={lastname}
+        />
+        <input
+          type="text"
+          name="lastname"
+          id="lastname"
+          onChange={(e) => setLastname(e.target.value)}
+          value={lastname}
           placeholder={userData.lastname}
-            />
-        <input className="btn-form" type="submit" value="Valider" />
-      </form>
-      <form action="" onSubmit={handleChangeEmail} className="mini-form">
-            <input
-              type="email"
-              name="email"
-              id="email"
-              onChange={(e) => setEmail(e.target.value)}
-              value={email}
-          placeholder={userData.email}
-            />
-        <input className="btn-form" type="submit" value="Valider" />
-      </form>
-      <form action="" onSubmit={handleChangeAdress} className="mini-form">
-            <input
+        />
+        <input
           type="text"
           name="adress"
           id="adress"
@@ -108,9 +76,6 @@ export default function UpdateProfil() {
           value={adress}
           placeholder={userData.adress}
         />
-        <input className="btn-form" type="submit" value="Valider" />
-      </form>
-      <form action="" onSubmit={handleChangePostalCode} className="mini-form">
         <input
           type="text"
           name="postalCode"
@@ -119,9 +84,6 @@ export default function UpdateProfil() {
           value={postalCode}
           placeholder={userData.postalCode}
         />
-        <input className="btn-form" type="submit" value="Valider" />
-      </form>
-      <form action="" onSubmit={handleChangeCity} className="mini-form">
         <input
           type="text"
           name="city"
@@ -129,9 +91,28 @@ export default function UpdateProfil() {
           onChange={(e) => setCity(e.target.value)}
           value={city}
           placeholder={userData.city}
-            />
-            <input className="btn-form" type="submit" value="Valider" />
-          </form>
+        />
+        <div>
+          Veuillez renseigner votre email et mot de passe pour tout chagement.
         </div>
+         <input
+          type="email"
+          name="email"
+          id="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          placeholder="Mot de passe"
+        />
+        <input className="btn-form" type="submit" value="Valider" />
+      </form>
+    </div>
   );
 }
--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -1,4 +1,4 @@
-import { GET_USER, UPDATE_EMAIL, UPDATE_LASTNAME, UPDATE_FIRSTNAME, UPDATE_ADRESS, UPDATE_POSTALCODE, UPDATE_CITY  } from "../actions/userAction";
+import { GET_USER, UPDATE_USER  } from "../actions/userAction";
 
 const initialState = {};
 
@@ -6,36 +6,8 @@ export default function userReducer(state = initialState, action) {
   switch (action.type) {
     case GET_USER:
       return action.payload;
-      case UPDATE_EMAIL:
-      return {
-        ...state,
-        email: action.payload,
-      };
-      case UPDATE_FIRSTNAME:
-      return {
-        ...state,
-        firstName: action.payload,
-      }
-      case UPDATE_LASTNAME:
-      return {
-        ...state,
-        lastname: action.payload,
-      }
-      case UPDATE_ADRESS:
-        return {
-          ...state,
-          adress: action.payload,
-        }
-        case UPDATE_POSTALCODE:
-          return {
-            ...state,
-            postalCode: action.payload
-          }
-          case UPDATE_CITY:
-      return {
-        ...state,
-        city: action.payload,
-      }
+      case UPDATE_USER:
+        return action.payload;
       default:
         return state
   }
